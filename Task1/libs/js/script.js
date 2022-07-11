b43fcc79510e32518b383cab8dc137069fc66886
Kashif Ameen <kashu@live.co.uk>
2022-07-11 15:09:31 +0100

    first commit

---
+++ Task1/libs/js/script.js
@@ -0,0 +1,32 @@
+$('#btnSubmit').click(function() {
+    
+    $.ajax({
+        url: "libs/php/getEarthquakes.php",
+        type: 'POST',
+        dataType: 'json',
+        data: {
+            north: $('#north').val(),
+            south: $('#south').val(),
+            east: $('#east').val(),
+            west: $('#west').val(),     
+        },
+       
+        
+        success: function(result) {
+            
+            console.log(JSON.stringify(result));
+
+            if (result.status.name == "ok") {
+                $('#listOfEarthquakes').html(result['data'][0]);
+            }
+        }
+    })
+    let str = $('#north').val()
+    
+    let str1 = $('#south').val()
+    let str2 = $('#west').val()
+    let str3 = $('#east').val()
+    console.log(`North is ${str}, South is ${str1}, East is ${str3}, West is ${str2}`)
+    alert(str1)
+})
+
