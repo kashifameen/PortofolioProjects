ffa45061619ab4177a33bb77021687b629eecc77
Kashif Ameen <kashu@live.co.uk>
2022-07-12 18:58:20 +0100

    added two more api's and minor styling

--- Task1/libs/js/script.js
+++ Task1/libs/js/script.js
@@ -1,4 +1,4 @@
-$('#btnSubmit').click(function() {
+$('#earthquakeBtn').click(function() {
     
     $.ajax({
         url: "libs/php/getEarthquakes.php",
@@ -10,22 +10,71 @@ $('#btnSubmit').click(function() {
             east: $('#east').val(),
             west: $('#west').val(),     
         },
-       
         
         success: function(result) {
             
             console.log(JSON.stringify(result));
 
             if (result.status.name == "ok") {
-                $('#listOfEarthquakes').html(result['data'][0]);
+                console.log('Working')
+                $('#listOfEarthquakes').html(result['data'][0]['datetime']);
+                $('#earthquakeMagnitude').html(result['data'][0]['magnitude']);
             }
         }
     })
-    let str = $('#north').val()
+})
+$('#oceanBtn').click(function() {
     
-    let str1 = $('#south').val()
-    let str2 = $('#west').val()
-    let str3 = $('#east').val()
-    alert(result['data'])
+    $.ajax({
+        url: "libs/php/getOceans.php",
+        type: 'POST',
+        dataType: 'json',
+        data: {
+            lat: $('#latitude').val(),
+            lng: $('#longitude').val(),     
+        },
+        
+        success: function(result) {
+            
+            console.log(JSON.stringify(result));
+               console.log(result.data.status.value)
+               if (result.data.status.value == 15) {
+                   console.log(result.data.status.message)
+                   $('#ocean').html(result.data.status.message)
+               }
+             else if (result.status.name == "ok") {
+                console.log('Working')
+                $('#nameOfOcean').html(result['data']['ocean']['name']);
+                console.log(result['data']['ocean'])
+                $('#geonameId').html(result['data']['ocean']['geonameId']);
+            }
+           
+            
+        }
+    })
+})
+$('#wikiBtn').click(function() {
+    
+    $.ajax({
+        url: "libs/php/getWikipediaSearch.php",
+        type: 'POST',
+        dataType: 'json',
+        data: {
+            q: $('#placename').val(),     
+        },
+        
+        success: function(result) {
+            console.log(result)
+            console.log(JSON.stringify(result));
+             if (result.status.name == "ok") {
+                console.log('Working')
+                $('#placenameEntered').html(result['data'][0]['title']);
+                $('#summary').html(result['data'][0]['summary']);
+            }
+           
+            
+        }
+        
+    })
 })
 
