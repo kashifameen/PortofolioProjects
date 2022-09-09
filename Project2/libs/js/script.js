8e7ecfc32582283ab18d863c977246d7b507d8e7
Kashif Ameen <kashu@live.co.uk>
2022-09-09 15:32:14 +0100

    initial layout complete

---
+++ Project2/libs/js/script.js
@@ -0,0 +1,35 @@
+$(document).ready(function(){
+	$.ajax({
+		url: "libs/php/getAll.php",
+		type: "GET",
+		dataType: "json",
+		success: function (result) {
+			console.log(result)
+			$.each(result.data, function (i, item) {
+				$('#tableBody').append(`<tr>
+				<td>${result.data[i].firstName}</td>
+				<td>${result.data[i].lastName}</td>                        
+				<td>${result.data[i].location}</td>
+				<td>${result.data[i].department}</td>
+				<td>${result.data[i].jobTitle}</td>
+				<td>${result.data[i].email}</td>
+				<td>
+					<a href="#" class="settings" title="Settings"  data-bs-toggle="modal" data-bs-target="#updateUserModal"><i class="material-icons">&#xE8B8;</i></a>
+					<a href="#" class="delete" title="Delete" ><i class="material-icons">&#xE5C9;</i></a>
+				</td>
+			</tr>
+				`);
+				
+
+				$('#updatefName').innerHTML= `<input type="text" class="form-control" id="updatefName" placeholder="${result.data[i].firstName}"></input>`;
+				$('#updatelName').innerHTML = result.data[i].lastName;
+				$('#updateLocation').val(result.data[i].location).change();
+				$('#updateJob').innerHTML = result.data[i].jobTitle;
+				$('#updateEmail').innerHTML = result.data[i].email;
+			})
+			
+			
+		}
+	})
+});
+
