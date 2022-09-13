90a6a1e1b22bc2fce70daa92a9251a52a9bf888f
Kashif Ameen <kashu@live.co.uk>
2022-09-13 12:44:59 +0100

    working on adding new personnel

--- Project2/libs/js/script.js
+++ Project2/libs/js/script.js
@@ -1,56 +1,79 @@
-$(document).ready(function(){
+$(document).ready(function () {
 	$.ajax({
-		url: "libs/php/getAll.php",
-		type: "GET",
-		dataType: "json",
-		success: function (result) {
-			console.log(result)
-			$.each(result.data, function (i, item) {
-				$('#tableBody').append(`<tr>
-				<td>${result.data[i].firstName}</td>
-				<td>${result.data[i].lastName}</td>                        
-				<td>${result.data[i].location}</td>
-				<td>${result.data[i].department}</td>
-				<td>${result.data[i].jobTitle}</td>
-				<td>${result.data[i].email}</td>
+        url: "libs/php/getAll.php",
+        type: "GET",
+        dataType: "json",
+        success: function (result) {
+            console.log(result)
+            $.each(result.data, function (i, item) {
+                $('#tableBody').append(`<tr>
+				<td>${
+                    result.data[i].firstName
+                }</td>
+				<td>${
+                    result.data[i].lastName
+                }</td>                        
+				<td>${
+                    result.data[i].location
+                }</td>
+				<td>${
+                    result.data[i].department
+                }</td>
+				<td>${
+                    result.data[i].jobTitle
+                }</td>
+				<td>${
+                    result.data[i].email
+                }</td>
 				<td>
 
-					<a href="#" class="settings" title="Settings" data-bs-toggle="modal" data-bs-target="#updateUserModal" data-personnelId=${result.data[i].id} ><i class="material-icons">&#xE8B8;</i></a>
+					<a href="#" class="settings" title="Settings" data-bs-toggle="modal" data-bs-target="#updateUserModal" id="button" data-personnelId=${
+                    result.data[i].id
+                } ><i class="material-icons">&#xE8B8;</i></a>
 					<a href="#" class="delete" title="Delete" ><i class="material-icons">&#xE5C9;</i></a>
 				</td>
 			</tr>
-				`)	
-				// document.getElementById('updatefName').value= result.data[i].firstName
-				// document.getElementById('updatelName').value = result.data[i].lastName;
-				// document.getElementById('updateLocation').value = result.data[i].location;
-				// document.getElementById('updateJob').value = result.data[i].jobTitle;
-				// document.getElementById('updateEmail').value = result.data[i].email;
-				// $('#updatelName').value = result.data[i].lastName;
-				// $('#updateLocation').val(result.data[i].location);
-				// $('#updateJob').val = result.data[i].jobTitle;
-				// $('#updateEmail').val = result.data[i].email;
-			})
-			
-			
-		}
-	})
-});
+				`)
+           })
+            $(".settings").on("click", function () {
+                let personnelId = $(this).attr("data-personnelId")
+                result.data.forEach(element => {
+                    if (element.id == personnelId) {
+                        console.log(element)
+                        document.getElementById('updatefName').value = element.firstName
+                        document.getElementById('updatelName').value = element.lastName;
+                        document.getElementById('updateLocation').value = element.location;
+                        document.getElementById('updateJob').value = element.jobTitle;
+                        document.getElementById('updateEmail').value = element.email;
+                        document.getElementById('updateDepartment').value = element.department
 
-$(".settings").on("click", function(){
-	console.log('This Works!')
-	alert($(this).attr("data-personnelid"))
-	
-	console.log($(this).attr("personnelid"))
-	$.ajax({
-		url:'libs/php/getPersonnelByID.php',
-		type:'GET',
-		dataType: 'json',
-		data: {
-			id: personnelId
-		},
-		success(result){
-			console.log(result)
-		}
-	})
+                    }
+                })
+            })
+        }
+    })
+	$("#addUserBtn").on("click", function(){
+		console.log('Testing!')
+		let addFirstName = document.getElementById('addFirstName').value;
+		let addLastName = document.getElementById('addLastName').value;
+		let addJob = document.getElementById('addJob').value;
+		let addEmail = document.getElementById('addEmail').value;
+		let addDepartment = document.getElementById('addDepartment').value;
+		$.ajax({
+			url:"libs/php/insertPersonnel.php",
+			type: "POST",
+			dataType: 'json',
+			data: {
+				firstName: addFirstName,
+				lastName: addLastName,
+				jobTitle: addJob,
+				email: addEmail,
+				departmentID: addDepartment,
+			},
+			success: function(result){
+				console.log('New User Added')
+			}
+		})
+})
 })
 
