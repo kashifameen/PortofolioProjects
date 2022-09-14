f1dd1d7266ae3a7f535e5b47fdd6666f8a763246
Kashif Ameen <kashu@live.co.uk>
2022-09-14 16:09:27 +0100

    completed filtering for location and department

--- Project2/libs/js/script.js
+++ Project2/libs/js/script.js
@@ -1,12 +1,7 @@
 $(document).ready(function () {
-    $.ajax({
-        url: "libs/php/getAll.php",
-        type: "GET",
-        dataType: "json",
-        success: function (result) {
-            console.log(result)
-            $.each(result.data, function (i, item) {
-                $('#tableBody').append(`<tr>
+    getAllPersonnel().done((result) =>{
+        $.each(result.data, function (i, item){
+            $('#tableBody').append(`<tr>
 				<td>${
                     result.data[i].firstName
                 }</td>
@@ -35,69 +30,67 @@ $(document).ready(function () {
                 } ><i class="material-icons">&#xE5C9;</i></a>
 				</td>
 			</tr>
-				`)
-            })
-            $(".settings").on("click", function () {
-                let personnelId = $(this).attr("data-personnelId")
-                result.data.forEach(element => {
-                    if (element.id == personnelId) {
-                        console.log(element)
-                        document.getElementById('updatefName').value = element.firstName
-                        document.getElementById('updatelName').value = element.lastName;
-                        // document.getElementById('updateLocation').value = element.location;
-                        document.getElementById('updateJob').value = element.jobTitle;
-                        document.getElementById('updateEmail').value = element.email;
-                        document.getElementById('updateDepartment').value = element.department
-
-                    }
-
-                })
-                $('#updateUserBtn').on("click", function () {
-                    let updateFirstName = document.getElementById('updatefName').value;
-                    let updateLastName = document.getElementById('updatelName').value;
-                    let updateJob = document.getElementById('updateJob').value;
-                    let updateEmail = document.getElementById('updateEmail').value;
-                    let updateDepartment = document.getElementById('updateDepartment').value;
-                    console.log(personnelId)
-                    $.ajax({
-                        url: "libs/php/updatePersonnel.php",
-                        type: "POST",
-                        dataType: 'json',
-                        data: {
-                            firstName: updateFirstName,
-                            lastName: updateLastName,
-                            jobTitle: updateJob,
-                            email: updateEmail,
-                            departmentID: updateDepartment,
-                            id: personnelId
-                        },
-                        success: function (result) {
-                            console.log(updateFirstName)
-                            document.getElementById('updateUserModalBody').innerHTML = `<h5>${updateFirstName}'s profile has been updated. </h5>`
-
-                        }
+				`) 
+        })
+    $(".settings").on("click", function () {
+        let personnelId = $(this).attr("data-personnelId")
+            result.data.forEach(element => {
+            if (element.id == personnelId) {
+                console.log(element)
+                document.getElementById('updatefName').value = element.firstName
+                document.getElementById('updatelName').value = element.lastName;
+                                // document.getElementById('updateLocation').value = element.location;
+                document.getElementById('updateJob').value = element.jobTitle;
+                document.getElementById('updateEmail').value = element.email;
+                document.getElementById('updateDepartment').value = element.department
+        
+                            }
+        
+                        })
                     })
-                })
-            })
+    
+    }) 
+    $('#updateUserBtn').on("click", function () {
+        let updateFirstName = document.getElementById('updatefName').value;
+        let updateLastName = document.getElementById('updatelName').value;
+        let updateJob = document.getElementById('updateJob').value;
+        let updateEmail = document.getElementById('updateEmail').value;
+        let updateDepartment = document.getElementById('updateDepartment').value;
+        console.log(personnelId)
+        $.ajax({
+            url: "libs/php/updatePersonnel.php",
+            type: "POST",
+            dataType: 'json',
+            data: {
+                firstName: updateFirstName,
+                lastName: updateLastName,
+                jobTitle: updateJob,
+                email: updateEmail,
+                departmentID: updateDepartment,
+                id: personnelId
+            },
+            success: function (result) {
+                document.getElementById('updateUserModalBody').innerHTML = `<h5>${updateFirstName}'s profile has been updated. </h5>`
 
-            $(".delete").on("click", function () {
-                let personnelId = $(this).attr("data-personnelId")
-                console.log(personnelId)
-                $.ajax({
-                    url: "libs/php/deletePersonnel.php",
-                    type: "POST",
-                    dataType: "json",
-                    data: {
-                        id: personnelId
-                    },
-                    success: function (result) {
-                        console.log(result)
-                    }
-                })
-            })
-        }
+            }
+        })
     })
-    $("#addUserBtn").on("click", function () {
+    $(".delete").on("click", function () {
+        let personnelId = $(this).attr("data-personnelId")
+        console.log(personnelId)
+        $.ajax({
+            url: "libs/php/deletePersonnel.php",
+            type: "POST",
+            dataType: "json",
+            data: {
+                id: personnelId
+            },
+            success: function (result) {
+                console.log(result)
+            }
+        })
+    })
+$("#addUserBtn").on("click", function () {
         console.log('Testing!')
         let addFirstName = document.getElementById('addFirstName').value;
         let addLastName = document.getElementById('addLastName').value;
@@ -122,4 +115,108 @@ $(document).ready(function () {
             }
         })
     })
-})
+    $('#locationForm').on("change", function(){
+        const chosenValue = this.value;
+        console.log(chosenValue)
+        $('#tableBody').empty()
+        getAllPersonnel().done((result) =>{
+            result.data.forEach(element => {
+                if(element.locationId == chosenValue){
+                    $('#tableBody').append(`<tr>
+                    <td>${
+                        element.firstName
+                    }</td>
+                    <td>${
+                        element.lastName
+                    }</td>                        
+                    <td>${
+                        element.location
+                    }</td>
+                    <td>${
+                        element.department
+                    }</td>
+                    <td>${
+                        element.jobTitle
+                    }</td>
+                    <td>${
+                        element.email
+                    }</td>
+                    <td>
+    
+                        <a href="#" class="settings" title="Settings" data-bs-toggle="modal" data-bs-target="#updateUserModal" id="button" data-personnelId=${
+                        element.id
+                    } ><i class="material-icons">&#xE8B8;</i></a>
+                        <a href="#" class="delete" title="Delete" data-personnelId=${
+                        element.id
+                    } ><i class="material-icons">&#xE5C9;</i></a>
+                    </td>
+                </tr>
+                    `) 
+                }
+            })
+        })
+    })
+    $('#departmentForm').on("change", function(){
+        const chosenValue = this.value;
+        console.log(chosenValue)
+        $('#tableBody').empty()
+        getAllPersonnel().done((result) =>{
+            result.data.forEach(element => {
+                if(element.departmentID == chosenValue){
+                    $('#tableBody').append(`<tr>
+                    <td>${
+                        element.firstName
+                    }</td>
+                    <td>${
+                        element.lastName
+                    }</td>                        
+                    <td>${
+                        element.location
+                    }</td>
+                    <td>${
+                        element.department
+                    }</td>
+                    <td>${
+                        element.jobTitle
+                    }</td>
+                    <td>${
+                        element.email
+                    }</td>
+                    <td>
+    
+                        <a href="#" class="settings" title="Settings" data-bs-toggle="modal" data-bs-target="#updateUserModal" id="button" data-personnelId=${
+                        element.id
+                    } ><i class="material-icons">&#xE8B8;</i></a>
+                        <a href="#" class="delete" title="Delete" data-personnelId=${
+                        element.id
+                    } ><i class="material-icons">&#xE5C9;</i></a>
+                    </td>
+                </tr>
+                    `) 
+                }
+            })
+        })
+    })
+})   
+    
+
+    
+   
+
+                
+            
+
+           
+      
+    
+    
+
+const getAllPersonnel = () => {
+    return $.ajax({
+        url: "libs/php/getAll.php",
+        type: "GET",
+        dataType: "json",
+        
+    });
+    
+}
